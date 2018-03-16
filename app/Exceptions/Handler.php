4822bfaea96c9b454cb7c3844b3ea6008ded50c1
Hubert Léo <leohub@live.fr>
2018-03-16 13:18:27 +0100

    Add Unauthorized handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,6 +48,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceof \Illuminate\Auth\AuthenticationException ){
+            return response(['status' => 'error', 'message' => 'Unauthorized'], 401);
+        }
         return parent::render($request, $exception);
     }
 }
