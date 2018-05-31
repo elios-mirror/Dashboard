f5adec734862186197d340611fa6cd5323250ee3
Hubert Léo <leohub@live.fr>
2018-05-31 17:05:03 +0200

    Add camera to default module

--- deploy.php
+++ deploy.php
@@ -1,8 +1,16 @@
 <?php
 namespace Deployer;
 
+use Deployer\Task\Context;
+use Deployer\Utility\Httpie;
+
+
 require 'recipe/laravel.php';
+require 'vendor/deployer/recipes/recipe/npm.php';
+
 // Project name
+
+// Discord config
 
 set('deploy_path', '/var/www/html');
 set('application', 'Emodyz Indexer');
@@ -44,10 +52,81 @@ task('build:js', function() {
     runLocally('npm run prod');
 });
 
+task('run:sockets', function() {
+    run('screen -dm bash -c "cd /var/www/html/release; npm install; killall nodejs;nodejs /var/www/html/release/sockets/development.js"');
+});
+
 // [Optional] if deploy fails automatically unlock
 //.
 after('deploy:failed', 'deploy:unlock');
 after('artisan:optimize', 'upload:env');
-after('deploy:prepare', 'build:js');
+//after('deploy:prepare', 'build:js');
 // Migrate database before symlink new release.
 before('deploy:symlink', 'artisan:migrate');
+after('deploy:update_code', 'run:sockets');
+
+// Discord notifications
+before('deploy:prepare', 'discord:notify');
+after('success', 'discord:notify:success');
+after('deploy:failed', 'discord:notify:failure');
+
+
+/**
+ * Discord
+ */
+
+set('discord_webhook', function () {
+    return 'https://discordapp.com/api/webhooks/449217228882313216/WNRX4Jhl45U5KZ7C2FImiR8l8nnsngMCGOcr_7F5E5TAKOWszLZim3urN_JI7Pe5MpRF';
+});
+
+// Deploy messages
+set('discord_notify_text', ':information_source: **{{user}}** is deploying branch `{{branch}}` to _{{target}}_');
+set('discord_success_text', ':white_check_mark: Branch `{{branch}}` deployed to _{{target}}_ successfully');
+set('discord_failure_text', ':no_entry_sign: Branch `{{branch}}` has failed to deploy to _{{target}}_');
+
+set('discord_type', 'discord_notify_text');
+
+// Helpers
+task('discord_send_message', function(){
+    Httpie::post(get('discord_webhook'))->body(['content' => get(get('discord_type'))])->send();
+});
+
+// Tasks
+desc('Just notify your Discord channel with all messages, without deploying');
+task('discord:test', function () {
+    set('discord_type', 'discord_notify_text');
+    invoke('discord_send_message');
+    set('discord_type', 'discord_success_text');
+    invoke('discord_send_message');
+    set('discord_type', 'discord_failure_text');
+    invoke('discord_send_message');
+})
+    ->once()
+    ->shallow();
+
+desc('Notify Discord');
+task('discord:notify', function () {
+    set('discord_type', 'discord_notify_text');
+    invoke('discord_send_message');
+})
+    ->once()
+    ->shallow()
+    ->isPrivate();
+
+desc('Notify Discord about deploy finish');
+task('discord:notify:success', function () {
+    set('discord_type', 'discord_success_text');
+    invoke('discord_send_message');
+})
+    ->once()
+    ->shallow()
+    ->isPrivate();
+
+desc('Notify Discord about deploy failure');
+task('discord:notify:failure', function () {
+    set('discord_type', 'discord_failure_text');
+    invoke('discord_send_message');
+})
+    ->once()
+    ->shallow()
+    ->isPrivate();
