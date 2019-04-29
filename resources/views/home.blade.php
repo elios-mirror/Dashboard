3f34c7101b8400f2a00ba5739bfb633bfd318f25
Frost <matthias.prost@epitech.eu>
2019-04-29 17:51:46 +0900

    Updated home design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,26 +8,43 @@
                     <div class="card-header">Your Applications</div>
 
                     <div class="card-body">
-                        @foreach($modules as $m)
-                            @if(auth()->user()->id === $m->publisher_id)
-                                <table class="table">
-                                    <tr>
-                                        <th>Title</th>
-                                        <th>Name</th>
-                                        <th>Repository</th>
-                                        <th>Description</th>
-                                        <th>Logo</th>
-                                    </tr>
-                                    <tr>
-                                        <td>{{ $m->title }}</td>
-                                        <td>{{ $m->name }}</td>
-                                        <td>{{ $m->repository }}</td>
-                                        <td>{{ $m->description }}</td>
-                                        <td><img src="{{ $m->logo_url }}"/ width="50" height="50"></td>
-                                    </tr>
-                                </table>
-                            @endif
-                        @endforeach
+                        <div class="container">
+                            <div class="row">
+                                @foreach($modules as $m)
+                                    @if(auth()->user()->id === $m->publisher_id)
+                                        <div class="col-md-3">
+                                            <div class="application-logo text-center">
+                                                <img src="{{ $m->logo_url }}" alt="application logo">
+                                            </div>
+                                            <div class="application-title text-center">
+                                                {{ $m->title }}
+                                            </div>
+                                            <div class="application-repository text-center">
+                                                <div class="text-center application-repository-content">
+                                                    {{ $m->repository }}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {{--<table class="table">
+                                            <tr>
+                                                <th>Title</th>
+                                                <th>Name</th>
+                                                <th>Repository</th>
+                                                <th>Description</th>
+                                                <th>Logo</th>
+                                            </tr>
+                                            <tr>
+                                                <td>{{ $m->title }}</td>
+                                                <td>{{ $m->name }}</td>
+                                                <td>{{ $m->repository }}</td>
+                                                <td>{{ $m->description }}</td>
+                                                <td><img src="{{ $m->logo_url }}" width="50" height="50"></td>
+                                            </tr>
+                                        </table>--}}
+                                    @endif
+                                @endforeach
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,6 @@
     </div>
 
     <!-- Scripts -->
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 
