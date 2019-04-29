17cae83148f38ec9a9eabb4b3bd17f9668e5c514
Frost <matthias.prost@epitech.eu>
2019-04-29 19:06:21 +0900

    Added logo + design changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
+                    <img src="/images/logo.png" alt="logo elios" width="30px" style="margin-right: .5rem;">
                     Elios Development
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
