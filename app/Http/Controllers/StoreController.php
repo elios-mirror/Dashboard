fa84b4662c81486ff817e688ba9263d1c2a1a0a9
Frost <matthias.prost@epitech.eu>
2019-05-03 15:51:54 +0900

    Added screenshots to store get highlight + lowercase for search

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -3,23 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Module;
+use App\ModuleScreenshots;
 use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-  public function index(Request $request)
-  {
-    $modules = Module::paginate();
-    return $modules;
-  }
+    public function index(Request $request)
+    {
+        $modules = Module::paginate();
 
-  public function search(Request $request)
-  {
-    $search = $request->get('q');
-    if (!$search) {
-      abort(400, "Missing 'q' query param ");
+        $modules->getCollection()->transform(function ($value) {
+            $screens = ModuleScreenshots::get();
+            $screen_array = array();
+
+            foreach ($screens as $screen) {
+                if ($value->id == $screen->module_id) {
+                    array_push($screen_array, $screen->screen_url);
+                }
+            }
+            $value->screenshots = $screen_array;
+
+            return $value;
+        });
+
+        return $modules;
     }
-    $modules = Module::where('name', 'LIKE', '%' . $search . '%')->orWhere('title', 'LIKE',  '%' . $search . '%')->paginate();
-    return $modules;
-  }
+
+    public function search(Request $request)
+    {
+        $search = $request->get('q');
+        $search = strtolower($search);
+
+        if (!$search) {
+            abort(400, "Missing 'q' query param ");
+        }
+        $modules = Module::where('name', 'LIKE', '%' . $search . '%')->orWhere('title', 'LIKE', '%' . $search . '%')->paginate();
+        return $modules;
+    }
 }
