d8799eaeb449704474a5bf25d34c62bf90c91130
Léo <leohub@live.fr>
2019-05-22 01:39:55 -0400

    Fix install Module 500

--- app/Http/Controllers/MirrorController.php
+++ app/Http/Controllers/MirrorController.php
@@ -216,7 +216,7 @@ class MirrorController extends Controller
         $mirror->link->modules()->attach($module->id, ['id' => $installId]);
         $mirror['modules'] = $mirror->link->modules()->with('module')->get();
         //$module = $mirror->link->modules()->where('mirror_modules.id', $installId)->first(); // TODO: See why i do that is past :-)
-        //$module->module;
+        $module->module;
         Notification::send($mirror, new MirrorInstalledModule($mirror, $request->user(), $module));
         return response()->json($mirror);
     }
