81d3aa823d26cf7c223e2151ef9dac790a8a0bc5
Léo <leohub@live.fr>
2019-05-22 02:05:09 -0400

    Realy Fix install Module 500 lol its a joke

--- database/seeds/DevUsersSeeder.php
+++ database/seeds/DevUsersSeeder.php
@@ -7,87 +7,104 @@ use App\Module;
 
 class DevUsersSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $user = User::create([
-            'email' => 'root@root.com',
-            'name' => 'Root',
-            'password' => bcrypt('root')
-        ]);
-        $user2 = User::create([
-            'email' => 'root@root.com2',
-            'name' => 'Root',
-            'password' => bcrypt('root')
-        ]);
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $user = User::create([
+        'email' => 'root@root.com',
+        'name' => 'Root',
+        'password' => bcrypt('root')
+    ]);
+    $user2 = User::create([
+        'email' => 'root@root.com2',
+        'name' => 'Root',
+        'password' => bcrypt('root')
+    ]);
 
 
-        DB::table('oauth_clients')->insert([
-            [
-                'name' => "Dev Grant Client",
-                'redirect' => 'http://localhost',
-                'personal_access_client' => 0,
-                'password_client' => 1,
-                'revoked' => 0,
-                'secret' => 'Rp52CEoYWjiIA0kRTTGspdbjee3tQxSaNCVn7J87'
-            ]
-        ]);
+    DB::table('oauth_clients')->insert([
+        [
+            'name' => "Dev Grant Client",
+            'redirect' => 'http://localhost',
+            'personal_access_client' => 0,
+            'password_client' => 1,
+            'revoked' => 0,
+            'secret' => 'Rp52CEoYWjiIA0kRTTGspdbjee3tQxSaNCVn7J87'
+        ]
+    ]);
 
-        DB::table('oauth_clients')->insert([
-            [
-                'name' => "Dev Grant Client",
-                'redirect' => 'http://localhost',
-                'personal_access_client' => 1,
-                'password_client' => 0,
-                'revoked' => 0,
-                'secret' => 'BKnWQrXAueBnxX0vHzcxjrmY5BoXl99Iy5Z11ene'
-            ]
-        ]);
+    DB::table('oauth_clients')->insert([
+        [
+            'name' => "Dev Grant Client",
+            'redirect' => 'http://localhost',
+            'personal_access_client' => 1,
+            'password_client' => 0,
+            'revoked' => 0,
+            'secret' => 'BKnWQrXAueBnxX0vHzcxjrmY5BoXl99Iy5Z11ene'
+        ]
+    ]);
 
-        DB::table('oauth_personal_access_clients')->insert([
-            [
-                'client_id' => 2,
-            ]
-        ]);
+    DB::table('oauth_personal_access_clients')->insert([
+        [
+            'client_id' => 2,
+        ]
+    ]);
 
-        $model = \App\MirrorModel::create([
-            'id' => 'LKD28376382'
-        ]);
-        
-        $mirror = Mirror::create([
-            'name' => 'Mirroir de test',
-            'ip' => "127.0.0.1",
-            'model' => $model->id
-        ]);
+    $model = \App\MirrorModel::create([
+        'id' => 'LKD28376382'
+    ]);
 
-        $user->mirrors()->attach($mirror->id);
+    $mirror = Mirror::create([
+        'name' => 'Mirroir de test',
+        'ip' => "127.0.0.1",
+        'model' => $model->id
+    ]);
 
-        $module1 = \App\Module::create([
-            'title' => 'ClockRemi - Test',
-            'name' => 'Elios_ClockTest',
-            'repository' => 'https://github.com/remigastaldi/Elios_ClockTest',
-            'category' => 'Entertainment',
-            'logo_url' => 'https://image.flaticon.com/icons/svg/1740/1740456.svg',
-            'publisher_id' => $user->id,
-            'description' => 'No description'
-        ]);
+    $user->mirrors()->attach($mirror->id);
 
-        $versionModule2_1 = \App\ModuleVersion::create([
-            'module_id' => $module1->id,
-            'commit' => '488bf5014821c9f4c52232a58b97b95bcda900ac',
-            'version' => '0.0.1',
-            'changelog' => 'First version'
-        ]);
+    $module1 = \App\Module::create([
+        'title' => 'Clock - Test',
+        'name' => 'Elios_ClockTest',
+        'repository' => 'https://github.com/remigastaldi/Elios_ClockTest',
+        'category' => 'Entertainment',
+        'logo_url' => 'https://image.flaticon.com/icons/svg/1740/1740456.svg',
+        'publisher_id' => $user->id,
+        'description' => 'No description'
+    ]);
 
-        $user->mirrors()->first()->link->modules()->attach($versionModule2_1->id);
+    $versionModule2_1 = \App\ModuleVersion::create([
+        'module_id' => $module1->id,
+        'commit' => '986d2ec4b3ab804c3aa5b210d5ce9d4ea2ed9c3',
+        'version' => '0.0.1',
+        'changelog' => 'First version'
+    ]);
 
-        $modulescreenshots = \App\ModuleScreenshots::create([
-            'module_id' => $module1->id,
-            'screen_url' => 'https://sapling-inc.com/wp-content/gallery/analog/Sapling-Analog-Round-Front-View-Dial-S-Hands-Standard.jpg',
-        ]);
-    }
+    $module2 = \App\Module::create([
+        'title' => 'Weather',
+        'name' => 'Weather',
+        'repository' => 'https://gitlab.elios-mirror.com/elios/Elios-Weather.git',
+        'category' => 'Entertainment',
+        'logo_url' => 'https://image.flaticon.com/icons/svg/1740/1740456.svg',
+        'publisher_id' => $user->id,
+        'description' => 'No description'
+    ]);
+
+    $versionModule2_2 = \App\ModuleVersion::create([
+        'module_id' => $module2->id,
+        'commit' => 'dad1bdf3249e8b5fcf4fa3b0f4591cc3c26da6b1',
+        'version' => '0.0.1',
+        'changelog' => 'First version'
+    ]);
+
+    $user->mirrors()->first()->link->modules()->attach($versionModule2_1->id, ['id' => 'f0cb5950-7c56-11e9-ba53-9d65019b8045']);
+
+    $modulescreenshots = \App\ModuleScreenshots::create([
+        'module_id' => $module1->id,
+        'screen_url' => 'https://sapling-inc.com/wp-content/gallery/analog/Sapling-Analog-Round-Front-View-Dial-S-Hands-Standard.jpg',
+    ]);
+  }
 }
