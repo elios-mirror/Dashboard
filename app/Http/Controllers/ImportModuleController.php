c66d88915585912a2d413c256794e97f6856ed24
Léo <leohub@live.fr>
2019-12-01 22:52:03 +0100

    Add import auto-redirection in update page if exist

--- app/Http/Controllers/ImportModuleController.php
+++ app/Http/Controllers/ImportModuleController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Module;
-use GitHub;
 
 class ImportModuleController extends Controller
 {
 
-  public function index() {
+  public function index(Request $request)
+  {
+    if ($request->has('json')
+        && $request->has('name')
+        && $request->has('version')) {
+      $user = $request->user();
+      $module = $user->publishedModules()->whereName($request->get('name'))->first();
+      if ($module) {
+        return redirect('/modules/updates/' . $module->id);
+      }
+    }
     return view('import-module');
   }
 }
