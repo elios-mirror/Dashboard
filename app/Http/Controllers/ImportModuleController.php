6b648410763fbf90ded559e4d838b73c5547805a
Léo <leohub@live.fr>
2019-12-02 20:42:41 +0100

    Add url encode

--- app/Http/Controllers/ImportModuleController.php
+++ app/Http/Controllers/ImportModuleController.php
@@ -15,9 +15,9 @@ class ImportModuleController extends Controller
       $user = $request->user();
       $module = $user->publishedModules()->whereName($request->get('name'))->first();
       if ($module) {
-        return redirect('/modules/updates/' . $module->id . '?json=' . $request->get('json') .
-            '&name=' . $request->get('name') .
-            '&version=' . $request->get('version'));
+        return redirect('/modules/updates/' . $module->id . '?json=' . urlencode($request->get('json')) .
+            '&name=' . urlencode($request->get('name')) .
+            '&version=' . urlencode($request->get('version')));
       }
     }
     return view('import-module');
