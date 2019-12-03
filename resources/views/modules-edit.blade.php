70f55883df8e18e88b8804dde2d7029afb860cf2
Vladislav LADOGE <vladislav.zobov@epitech.eu>
2019-12-03 15:02:41 +0100

    change email for title

--- resources/views/modules-edit.blade.php
+++ resources/views/modules-edit.blade.php
@@ -30,7 +30,7 @@
 
                             <div class="row">
                                 <div class="form-group col-md-12">
-                                    <label for="email">Title</label>
+                                    <label for="title">Title</label>
                                     <input type="text" class="form-control" name="title" value="{{$module->title}}">
                                 </div>
                             </div>
